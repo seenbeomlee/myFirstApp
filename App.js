2a2b2821de865f8517093c0430389ef30dcd2964
Seenbeom Lee <mail@seenblee.com>
2020-10-10 21:58:02 +0900

    #1.1 React-Native basic components
    > button
    > input
    > event

--- App.js
+++ App.js
@@ -1,14 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput, Alert } from 'react-native';
 
 export default function App() {
+  let content = '';
   return (
     <View style={styles.container}>
-      <View style={styles.box}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <TextInput
+        placeholder="input text"
+        onChangeText={(text) => {
+          content = text;
+        }}
+        style={styles.input}
+      />
+      <TouchableOpacity
+        activeOpacity={0.8}
+        onPress={() => {
+          Alert.alert(content);
+        }}
+      >
+        <View style={styles.box}>
+          <Text style={styles.first}>Start</Text>
+          <StatusBar style="auto" />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -21,6 +36,19 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   box: {
-    backgroundColor: 'blue',
+    flexDirection: 'row',
+    backgroundColor: '#6830CF',
+    padding: 16,
+  },
+  first: {
+    fontSize: 20,
+    color: '#FFFFFF',
+    fontWeight: '600',
+    padding: 16,
+  },
+  input: {
+    width: '100%',
+    textAlign: 'center',
+    marginBottom: 16,
   },
 });
